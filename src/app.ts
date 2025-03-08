65f09b3bf5a2d3939a3183abfad5ecfad4a0408c
Kayene-Teixeira <kayeneteixeira490@gmail.com>
2025-03-07 21:01:56 -0300

    ✨feat: implementando cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express"; // quick fix pra instalar a tipagem do express para o typescript
 import router from "./routes";
+import cors from "cors";
 
 // configurando app 
 function createApp() {
@@ -9,6 +10,12 @@ function createApp() {
     // define que o corpo da requisição vai ser json 
     app.use(express.json());
     app.use('/api', router); // le arquivo de rotas
+
+    const corsOptions = {
+        origin: '*',
+    }
+
+    app.use(cors());
 
     return app;
 }
